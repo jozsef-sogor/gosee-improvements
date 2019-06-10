903cbde029fe12822d70a70d4e372083e544b840
Eduard <edward.roman8@gmail.com>
2019-06-10 16:38:36 +0200

    Making the Premium Table

--- scipt.js
+++ scipt.js
@@ -3,19 +3,19 @@ $(document).ready(function () {
 
     $('nav').hide();
 
-    $("#myVideo").get(0).play();
-
-    $(".mute-video").click(function () {
-        if ($("video").prop('muted')) {
-            $("video").prop('muted', false);
-            $(this).addClass('unmute-video');
-
-        } else {
-            $("video").prop('muted', true);
-            $(this).removeClass('unmute-video');
-        }
-        console.log($("video").prop('muted'))
-    });
+//    $("#myVideo").get(0).play();
+//
+//    $(".mute-video").click(function () {
+//        if ($("video").prop('muted')) {
+//            $("video").prop('muted', false);
+//            $(this).addClass('unmute-video');
+//
+//        } else {
+//            $("video").prop('muted', true);
+//            $(this).removeClass('unmute-video');
+//        }
+//        console.log($("video").prop('muted'))
+//    });
 
 
 //navigation
