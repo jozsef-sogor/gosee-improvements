3a22a5dd19404d63ec2fc0134a446d98b6fc003b
Adi George <adi_george@outlook.com>
2019-06-16 12:58:31 +0200

    slideshow

--- scipt.js
+++ scipt.js
@@ -164,10 +164,41 @@ $(document).ready(function () {
         });
     });
 
-
-
-
-
-
-
 });
+
+
+
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 3000;	// Time Between Switch
+
+
+// Image List
+images[0] = "images/frame1.png";
+images[1] = "images/frame2.png";
+
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+    
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++; 
+	} else { 
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
+
+
+
+
